d1ac7bcd80a288bf2fbc54022eb2bf328d467f97
Ammar0deh <100079767+Ammar0deh@users.noreply.github.com>
2023-12-26 01:51:10 +0300

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,8 @@
-jQuery(document).ready(function($) {
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
-
-});
+jQuery(document).ready(function() {
+    jQuery('.navTrigger').click(function() {
+        jQuery(this).toggleClass('active');
+        console.log("Clicked menu");
+        jQuery("#mainListDiv").toggleClass("show_list");
+        jQuery("#mainListDiv").fadeIn();
+    });
 });
